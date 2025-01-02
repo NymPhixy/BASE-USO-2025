eecd7dc2a990639437deb830585b1abe8c36cc42
Alexander Janssen <ajn.janssen@gmail.com>
2025-01-02 15:03:31 +0100

    onboarding

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+
+
 // Show loader on navigation
 const links = document.querySelectorAll("a");
 links.forEach((link) => {
@@ -33,5 +35,43 @@ links.forEach((link) => {
     }
   });
 });
+
+// json fetch languages
+  // Fetch the JSON data
+  fetch('config/language/language.json')
+  .then(response => response.json())
+  .then(data => {
+    const countrySelect = document.getElementById('country');
+    data.countries.forEach(country => {
+      const option = document.createElement('option');
+      option.value = country.value;
+      option.textContent = country.name;
+      countrySelect.appendChild(option);
+    });
+  })
+  .catch(error => console.error('Error fetching country data:', error));
+
+  // show form
+  document.addEventListener("DOMContentLoaded", function () {
+    const heroContentContainer = document.getElementById("hero-content-container");
+    const aanmeldFormulier = document.getElementById("aanmeld-formulier");
+    const heroButton = document.querySelector(".hero-button");
+    const resetButton = aanmeldFormulier.querySelector("input[type='reset']");
+
+    // Hide the form by default
+    aanmeldFormulier.style.display = "none";
+
+    heroButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      heroContentContainer.style.display = "none";
+      aanmeldFormulier.style.display = "block";
+    });
+
+    resetButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      aanmeldFormulier.style.display = "none";
+      heroContentContainer.style.display = "block";
+    });
+  });
 
 
